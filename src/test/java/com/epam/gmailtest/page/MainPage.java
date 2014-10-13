a7c7ab040fe0dfce04d12dc0700ed20d70d0daa3
Kanstantsin <Kanstantsin_Makarau@epam.com>
2014-10-13 23:06:11 +0300

    Merge branch 'master' of D:\Epam\AutomationLab\GmailTest with conflicts.

--- src/test/java/com/epam/gmailtest/page/MainPage.java
+++ src/test/java/com/epam/gmailtest/page/MainPage.java
@@ -9,8 +9,6 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
 /**
  * Created by Kanstantsin_Makarau on 10/9/2014.
  */
@@ -30,28 +28,21 @@ public class MainPage {
     @FindBy(xpath = "//body[@class='editable LW-avf']")
     private WebElement inputMessage;
 
-    @FindBy(xpath = "//div[@class='T-I J-J5-Ji aoO T-I-atl L3']")
+    //todo english language
+    @FindBy(xpath = "//div[@id=':8y']")
     private WebElement buttonSend;
 
     @FindBy(xpath = "//tr[@class='zA zE']")
     private WebElement unreadMessage;
 
-    @FindBy(xpath = "//tr[@class='zA yO']")
-    private WebElement readMessage;
-
-    @FindBy(css = "div.T-Jo-auh")
-    private WebElement checkBox;
-
-    @FindBy(css = "div.asl.T-I-J3.J-J5-Ji")
+    //todo correct xpath
+    @FindBy(xpath = "//div[@class='asa']")
     private WebElement buttonToSpam;
 
-    @FindBy(xpath = "//div[text()='Delete forever/..'")
-    private WebElement buttonDelete;
-
-    @FindBy(xpath = "//input[@class='gbqfif']")
+    @FindBy(xpath = "//input[@id='gbqfq']")
     private WebElement inputFind;
 
-    @FindBy(xpath = "//button[@class='gbqfb']")
+    @FindBy(xpath = "//button[@id='gbqfb']")
     private WebElement buttonSearch;
 
     public MainPage(WebDriver driver) {
@@ -68,15 +59,15 @@ public class MainPage {
         inputMessage.sendKeys(message);
         driver.switchTo().parentFrame();
         buttonSend.click();
-        logger.info("message was written");
+        logger.info("message written");
     }
 
     public void markMessageLikeSpam() {
         logger.info("try to mark message like spam...");
-        new WebDriverWait(driver, 30).until(ExpectedConditions.visibilityOf(unreadMessage));
-        unreadMessage.findElement(By.cssSelector("div.T-Jo-auh")).click();      //Inbox checkbox
+        new WebDriverWait(driver, 15).until(ExpectedConditions.visibilityOf(unreadMessage));
+        unreadMessage.click();
         buttonToSpam.click();
-        logger.info("massage was marked");
+        logger.info("massage marked");
     }
 
     public boolean checkSpamFolder() {
@@ -84,27 +75,13 @@ public class MainPage {
         inputFind.sendKeys("in:spam");
         buttonSearch.click();
         try {
-            //todo должен искать два сообщения
-            List<WebElement> unreadSpamMessages = unreadMessage.findElements(By.xpath("//span[@email='epamlab.user1@gmail.com']"));
-            logger.info("Found messages from spammer : " + unreadSpamMessages);
-            if(unreadSpamMessages.size()>1){
-                return true;
-            }
-            else return false;
+            logger.info("Found message from spammer : " +
+                    unreadMessage.findElement(By.xpath("//span[@email='epamlab.user1@gmail.com']")));
+            return true;
         }
         catch(Exception e){
             logger.error(e.getMessage());
             return false;
         }
-    }
-
-    public void deleteSpamMessages() {
-        logger.info("try to delete messages from our spammer...");
-        List<WebElement> readMessages = readMessage.findElements(By.xpath("//span[@email='epamlab.user1@gmail.com']"));
-        for(WebElement currentMessage : readMessages){
-            currentMessage.findElement(By.xpath("/../..//td[@class='oZ-x3 xY']")).click();      //Spam checkbox
-        }
-        buttonDelete.click();
-        logger.info("Spam messages from our spammer was deleted");
     }
 }
